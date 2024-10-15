366ebbc2081fc5d4613376d809936b1d8b26ae97
Ethan Beach <Beached@mail.uc.edu>
2024-10-15 11:37:53 -0400

    Add project files.

---
+++ Group5_Final_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group5_Final_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
