446c2bc2967babbeb7019f7c1f098a6ad5cd7c1c
Ethan <Beached@mail.uc.edu>
2024-11-03 14:49:45 -0500

    Added starting models + Updated appsettings.json

---
+++ Group5_Final_Project/Models/Developer.cs
@@ -0,0 +1,9 @@
+﻿namespace Group5_Final_Project.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; } //Ex: Artist, Programmer, etc
+    }
+}
---
+++ Group5_Final_Project/Models/GameGenre.cs
@@ -0,0 +1,8 @@
+﻿namespace Group5_Final_Project.Models
+{
+    public class GameGenre
+    {
+        public int Id { get; set; }
+        public string Genre { get; set; }
+    }
+}
