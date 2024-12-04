a578ae163f33e69085af2bb3ebc06c2cfd475b29
AGrosko <131299398+AGrosko@users.noreply.github.com>
2024-12-04 13:17:06 -0500

    Completed Developer and Platform controllers

--- Group5_Final_Project/Controllers/DeveloperController.cs
+++ Group5_Final_Project/Controllers/DeveloperController.cs
@@ -19,5 +19,46 @@ namespace Group5_Final_Project.Controllers
             return View(developers);
         }
 
+        [HttpGet]
+        public IActionResult Add(){
+            ViewBag.Action = "Add";
+            return View("Edit", new Developer());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id){
+            ViewBag.Action = "Edit";
+            var developer = _context.Developers.Find(Id);
+            return View(developer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Developer developer){
+            if (ModelState.IsValid){
+                if (developer.Id == 0){
+                    _context.Developers.Add(developer);
+                }else{
+                    _context.Developers.Update(developer);
+                }
+                _context.SaveChanges();
+                return RedirectToAction("Developers");
+            }else{
+                ViewBag.Action = (developer.Id ==0) ? "Add" : "Edit";
+                return View(developer);
+                 }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id){
+            var developer = _context.Developers.Find(id);
+            return View(developer);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Developer developer){
+            _context.Developers.Remove(developer);
+            _context.SaveChanges();
+            return RedirectToAction("Developers");
+        }
     }
 }
