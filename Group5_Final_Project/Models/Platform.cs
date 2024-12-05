494a924a7b919ad138c55bd165a37ddd99ac2bfe
AGrosko <131299398+AGrosko@users.noreply.github.com>
2024-12-04 22:35:31 -0500

    added functionality to the platforms page

--- Group5_Final_Project/Models/Platform.cs
+++ Group5_Final_Project/Models/Platform.cs
@@ -1,8 +1,11 @@
-﻿namespace Group5_Final_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Group5_Final_Project.Models
 {
     public class Platform
     {
         public int Id { get; set; }
+        [Required (ErrorMessage ="Please enter a Platform Name")]
         public string? Name { get; set; }
     }
 }
