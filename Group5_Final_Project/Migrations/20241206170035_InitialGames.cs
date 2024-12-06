5644e343d67998000249b509f59b48149cfda4d8
NathanDahlquist <nathan.j.dahlquist@gmail.com>
2024-12-06 12:47:01 -0500

    I cannot get the database locally, so this needs to be checked.

---
+++ Group5_Final_Project/Migrations/20241206170035_InitialGames.cs
@@ -0,0 +1,173 @@
+﻿Updateusing System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Group5_Final_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Developers_DeveloperId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Genres_GenreId",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Platforms",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ReleaseDate",
+                table: "Games",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeveloperId",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Developers_DeveloperId",
+                table: "Games",
+                column: "DeveloperId",
+                principalTable: "Developers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Genres_GenreId",
+                table: "Games",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Developers_DeveloperId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Genres_GenreId",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Platforms",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ReleaseDate",
+                table: "Games",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Games",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Games",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeveloperId",
+                table: "Games",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Developers_DeveloperId",
+                table: "Games",
+                column: "DeveloperId",
+                principalTable: "Developers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Genres_GenreId",
+                table: "Games",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id");
+        }
+    }
+}
--- Group5_Final_Project/Models/GameGenre.cs
+++ Group5_Final_Project/Models/GameGenre.cs
@@ -1,8 +1,11 @@
-﻿namespace Group5_Final_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Group5_Final_Project.Models
 {
     public class GameGenre
     {
         public int Id { get; set; }
+        [Required (ErrorMessage="Please Enter a Genre")]
         public string? Genre { get; set; }
     }
 }
